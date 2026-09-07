2fd99153e8570ca6a969ac1f40fff9a93a4be40c
ryan-carlo-githu <ryancarlo080@gmail.com>
2026-09-07 09:06:10 -0300

    Incluindo aula de condições com 'if', que ainda esta em andamento

---
+++ if/if.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+	// ESTUDO DE OPERADORES RELACIONAIS
+	// Comando if: se a condição é verdadeira, o bloco é executado, senão, é ignorao.
+	
+	//    SINTAXE:
+	//    if (<condicao>){
+	//	      <bloco_de_comandos
+	//    }
+	
+	// OPERADORES
+	// Maior = >
+	// Maior ou igual = >=
+	// Menor = <
+	// Menor ou igual: <=
+	// Igual = ==
+	// Diferente: !=
+
+int main (){
+	
+	// Inserção de variáveis
+	float m;
+	
+	// Entrada de dados
+	printf("Insira a nota:");
+	scanf("%f", &m);
+	
+	// Verificação de condição
+	if(m >= 7.0){
+		// Retorno caso a condição seja verdadeira
+		printf("Aprovado(a)!\n");
+	}
+	
+		
+	return 0;
+		
+}
